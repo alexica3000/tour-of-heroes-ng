b1c18dd4aeb6242082c5ab64ee4c29452b2bab80
Alex S <sanatatea@yandex.com>
2022-07-21 11:49:27 +0300

    test rxjs component

--- src/app/components/rxjs-test/rxjs-test.component.ts
+++ src/app/components/rxjs-test/rxjs-test.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {from, of} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {from, interval, of, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-rxjs-test',
   templateUrl: './rxjs-test.component.html',
   styleUrls: ['./rxjs-test.component.css']
 })
-export class RxjsTestComponent implements OnInit {
+export class RxjsTestComponent implements OnInit, OnDestroy {
+  intervalSubscription$: Subscription;
+  currentNumber: number = 0;
 
-  constructor() { }
+  constructor() {
+    this.intervalSubscription$ = new Subscription();
+  }
 
   ngOnInit(): void {
-    of(2, 4, 6).subscribe(item => console.log(item));
+    of(2, 4, 6).subscribe(item => console.log(`of .. ${item}`));
+
     from(['a', 'b', 'c', 'd'])
       .subscribe({
-        next: item => console.log(`resulting item ${item}`),
+        next: item => console.log(`from .. ${item}`),
         error: err => console.log(`error occurring ${err}`),
         complete: () => console.log('complete')
       });
+
+    this.intervalSubscription$ = interval(3000).subscribe(item => {
+      this.currentNumber = item;
+      console.log(`interval .. ${item}`);
+    });
+  }
+
+  ngOnDestroy() {
+    this.intervalSubscription$.unsubscribe();
   }
 }
